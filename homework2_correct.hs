7fafc62d67079520c0c72f7b501afd2815bb6f80
artys <a.kyrgys@g.nsu.ru>
2018-10-02 21:20:21 +0700

    create project

---
+++ homework2_correct.hs
@@ -0,0 +1,10 @@
+quadraticSolver::Float->Float->Float->(Float, Float)
+quadraticSolver a b c   |a==0 && b/=0 = (cdivb, cdivb)
+						|a==0 && b==0 = error "Not correct"
+						|d>=0 = (x1, x2)
+						|otherwise = error "No real roots"
+						where
+                         d = b^2-4*a*c
+                         x1 = (-b+sqrt(d))/(2*a)
+                         x2 = (-b-sqrt(d))/(2*a)
+                         cdivb = (-b)/c
