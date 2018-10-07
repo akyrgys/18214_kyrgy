c1cd4b3a272e3d4992c32513a5f9eac52d4272b8
akyrgys <42936641+akyrgys@users.noreply.github.com>
2018-10-07 22:46:36 +0700

    Update homework3.hs

--- homework3.hs
+++ homework3.hs
@@ -3,3 +3,5 @@ myfoldrmap f xs = foldr (\x xs -> f x:xs) [] xs
 
 myfoldlmap :: (a -> b) -> [a] -> [b]
 myfoldlmap f xs = foldl (\xs x -> xs ++ [f x]) [] xs
+
+--foldr в данном случае более эффективен, т.к. в нем испольхуется операция (:) - эффективней, чем (++), использующаяся в foldl
